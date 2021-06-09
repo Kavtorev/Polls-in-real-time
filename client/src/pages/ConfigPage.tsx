79cf0b5582287c89c070388b2f51b48050ea4ee1
Kavtorev <dmitrijkavtorev@gmail.com>
2021-06-09 11:11:43 +0200

    copy to the clipboard on click

--- client/src/pages/ConfigPage.tsx
+++ client/src/pages/ConfigPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { usePollContext } from "../globalProvider";
 import { OptionHolder } from "../components/OptionHolder";
 import { OptionsList } from "../components/OptionsList";
@@ -8,8 +8,8 @@ import { validateInitPage } from "./InitPage";
 import { ContinueButton } from "../components/ContinueButton";
 import { Truncate } from "../components/Truncate";
 import { Toolbar } from "../components/Toolbar";
-import { Button as StyledButton } from "../components/StyledButton";
-import FileCopyIcon from "@material-ui/icons/FileCopy";
+import { StyledButton } from "../components/StyledButton";
+import { toast } from "react-toastify";
 import Tooltip from "@material-ui/core/Tooltip";
 import ShareIcon from "@material-ui/icons/Share";
 
@@ -38,15 +38,16 @@ const StyledLinkHolder = styled.div<StyledCopyButtonProps>`
   border: 2px solid var(--primary-button-backgroundColor);
   margin-bottom: var(--top-bottom-margin);
   margin-right: 0.3em;
+  cursor: pointer;
   ${(props) =>
     props.active
-      ? "color: var(--primary-button-color); border: 2px solid var(--primary-button-backgroundColor);"
+      ? "color: var(--primary-button-backgroundColor); border: 2px solid var(--primary-button-backgroundColor);"
       : "color: var(--inactive-dark-color); border: 2px solid var(--inactive-dark-color);"};
   border-radius: 2px;
   transition: all 1s;
 
   :hover {
-    box-shadow: inset 0 0 6px -2px rgba(0, 0, 0, 0.2);
+    box-shadow: inset 0 0 5px -2px rgba(0, 0, 0, 0.2);
   }
 `;
 
@@ -71,12 +72,21 @@ const StyledLinkButton = styled(StyledButton)`
   }
 `;
 
-const StyledCopyButton = styled(StyledLinkButton)``;
-
 const StyledShareButton = styled(StyledLinkButton)``;
 
 export const ConfigPage: React.FC = ({ children }) => {
   let { state } = usePollContext();
+  let linkHolderRef = useRef<HTMLDivElement>(null);
+
+  const handleLinkTextClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (state.invitationalLink) {
+      navigator.clipboard.writeText(state.invitationalLink).then(
+        () => toast("Successfully copied to the clipboard"),
+        () => toast("Please copy the link manually")
+      );
+    }
+  };
+
   return (
     <>
       {validateInitPage(state) ? (
@@ -87,15 +97,13 @@ export const ConfigPage: React.FC = ({ children }) => {
             <OptionsList />
             <OptionHolder />
             <StyledConfigFooter>
-              <StyledLinkHolder active={false}>
-                <StyledLinkText>
-                  http://localhost:3000/2@^#2g3qc2@^#2g3qc2@^#2g3qc
+              <StyledLinkHolder
+                active={!!state.invitationalLink}
+                ref={linkHolderRef}
+              >
+                <StyledLinkText onClick={handleLinkTextClick}>
+                  {state.invitationalLink}
                 </StyledLinkText>
-                <Tooltip title="Copy the link">
-                  <StyledCopyButton>
-                    <FileCopyIcon />
-                  </StyledCopyButton>
-                </Tooltip>
                 <Tooltip title="Share the link">
                   <StyledShareButton>
                     <ShareIcon />
