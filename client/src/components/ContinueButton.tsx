17d750738cabc122ca3c6c6347a88f20dadad025
Kavtorev <dmitrijkavtorev@gmail.com>
2021-06-21 16:58:30 +0200

    ContinueButton update

--- client/src/components/ContinueButton.tsx
+++ client/src/components/ContinueButton.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import { StyledButton } from "./StyledButton";
 import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
 
-export const ContinueButton: React.FC = () => {
+export const ContinueButton: React.FC<{
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+}> = ({ onClick = () => null, disabled = false }) => {
   return (
-    <StyledButton endIcon={<ArrowForwardIosRoundedIcon />} type="submit">
+    <StyledButton
+      endIcon={<ArrowForwardIosRoundedIcon />}
+      type="submit"
+      disabled={disabled}
+      onClick={onClick}
+      alignRight={true}
+    >
       Continue
     </StyledButton>
   );
