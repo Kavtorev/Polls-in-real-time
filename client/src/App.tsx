c528012bff47a399c6ac3a12592c3c5111416df7
Kavtorev <dmitrijkavtorev@gmail.com>
2021-07-01 13:53:30 +0200

    +freedom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -57,18 +57,14 @@ const App: React.FC = () => {
               key={location.key}
             >
               <Switch location={location}>
-                <Route path="/poll/:id">
-                  <PollPage />
-                </Route>
-
-                {state.sessionID && !location.pathname.startsWith("/poll") ? (
-                  <Redirect to={`/poll/${state.sessionID}`} />
-                ) : null}
                 <Route path="/" exact>
                   <InitPage />
                 </Route>
                 <Route path="/config">
                   <ConfigPage />
+                </Route>
+                <Route path="/poll/:id">
+                  <PollPage />
                 </Route>
                 <Redirect to="/" />
               </Switch>
